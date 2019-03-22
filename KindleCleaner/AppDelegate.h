e5f2e120782b497e6a16e8bf561c7e7a2169b347
༺ Yuri ༒ Boyka ༻ <lilung.chao@gmail.com>
2019-03-22 09:20:11 +0800

    commit the kindle cleaner project

---
+++ KindleCleaner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KindleCleaner
+//
+//  Created by Yuri Boyka on 2019/3/22.
+//  Copyright © 2019 Yuri Boyka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KindleCleaner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KindleCleaner
+//
+//  Created by Yuri Boyka on 2019/3/22.
+//  Copyright © 2019 Yuri Boyka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
